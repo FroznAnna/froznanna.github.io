214600e7f3f91787b652c846436f7b866c179170
FroznAnna <64392141+FroznAnna@users.noreply.github.com>
2025-11-08 16:00:05 +0000

    Update page for no id, invalid id
    Added another recipe too

--- src/recipes/generator.js
+++ src/recipes/generator.js
@@ -3,15 +3,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const recipeSlug = urlParams.get('id');
 
-    if (!recipeSlug) {
-        // Handle case where no ID is provided (e.g., show an error or list of recipes)
-        document.getElementById('title').textContent = "Recipe Not Found";
-        return; 
+    function hideAll() { //Hides divs and br, whilst keeping header, home button and background
+        let divs = document.querySelectorAll('div');
+        divs.forEach(div => {
+            div.style.display = 'none';
+        })
+
+        let gaps = document.querySelectorAll('br');
+        gaps.forEach(gap => {
+            gap.style.display = 'none';
+        })
+    }
+
+    function redirect(id) {
+        window.location.href = `https://froznanna.github.io/recipe?id=${id}`;
     }
 
     fetch('https://froznanna.github.io/src/recipes/recipes.json')
         .then(response => response.json())
         .then(data => {
+            if (!recipeSlug) { //If no ID is provided - show list of all recipes
+                hideAll();
+                document.getElementById('header').textContent = "Recipes";
+                data.forEach(recipe => {
+                    let button = document.createElement('button');
+                    button.title = recipe.title;
+                    button.textContent = recipe.title;
+                    button.addEventListener('click', () => redirect(recipe.slug));
+                    document.body.appendChild(button);
+                })
+                return;
+            }
+
             const recipe = data.find(r => r.slug === recipeSlug);
 
             if (recipe) {
@@ -39,7 +62,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 createList(recipe.notes, 'notes');
 
             } else {
+                hideAll();
                 document.getElementById('title').textContent = "Recipe Not Found";
+                document.getElementById('header').textContent = "Recipe Not Found";
+                let warning = document.createElement('p');
+                warning.textContent = "Recipe does not exist";
+                warning.classList.add('subtitle');
+                document.body.appendChild(warning);
             }
         })
         .catch(error => console.error('Error fetching data:', error));
